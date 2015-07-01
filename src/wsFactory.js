56b754f972232759f4a736bee8743e5a58fe9c58
Jan Maselj <jan.maselj@ibzoot.com>
2015-07-01 14:33:40 +0200

    factory to oop client

--- src/wsFactory.js
+++ src/wsFactory.js
@@ -19,17 +19,18 @@
             callbacks: {}
         };
 
-        var wsClient = function (_options) {
+        return function (_options) {
             // https://docs.angularjs.org/api/ng/function/angular.extend
-            this.options = angular.extend({}, defaults, _options);
-            this.subscribers = [];
-            this.reconnectTimeout = null;
-
+            var options = angular.extend({}, defaults, _options);
             // this gets populated by WebSocket obj
-            this.socket = null;
+            var socket = null;
+
+            var subscribers = [];
+            var reconnectTimeout = null;
+
 
             this.connect = function () {
-                if (!this.options.url) {
+                if (!options.url) {
                     $log.error('wsClient connection URL not defined!');
                     return;
                 }
@@ -38,70 +39,68 @@
             };
 
             this.send = function (message) {
-                this.socket.send(message);
+                socket.send(message);
             };
 
             // https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent
             this.close = function (code, reason) {
                 // we don't want to reconnect when manual close
-                this.options.reconnect = false;
-                this.socket.close(code, reason);
+                options.reconnect = false;
+                socket.close(code, reason);
             };
 
             this.subscribe = function (subscriber, callback) {
-                $log.debug('new subscriber [' + subscriber + '] to socket [' + this.options.url + ']');
-                this.subscribers[subscriber] = callback;
+                $log.debug('new subscriber [' + subscriber + '] to socket [' + options.url + ']');
+                subscribers[subscriber] = callback;
             };
 
             this.unsubscribe = function (subscriber) {
-                $log.debug('unsubscribing [' + subscriber + '] from socket [' + this.options.url + ']');
-                delete this.subscribers[subscriber];
+                $log.debug('unsubscribing [' + subscriber + '] from socket [' + options.url + ']');
+                delete subscribers[subscriber];
             };
+
+            function connect() {
+                socket = new WebSocket(options.url);
+                socket.onopen = onOpen;
+                socket.onclose = onClose;
+                socket.onmessage = onMessage;
+                socket.onerror = onError;
+            }
+
+            function onOpen(event) {
+                $log.debug('wsClient connected to [' + options.url + ']');
+                if (reconnectTimeout) {
+                    $timeout.cancel(reconnectTimeout);
+                }
+            }
+
+            function onClose(event) {
+                $log.debug('wsClient connection to [' + options.url + '] closed');
+                if (reconnectTimeout) {
+                    $timeout.cancel(reconnectTimeout);
+                }
+                if (options.reconnect) {
+                    $log.debug('wsClient will try to reconnect in [' + options.reconnectIntervalTimeout + '] ms');
+                    reconnectTimeout = $timeout(connect, options.reconnectIntervalTimeout);
+                }
+            }
+
+            function onMessage(event) {
+                $log.debug('wsClient connection to [' + options.url + '] message received, will notify listeners');
+                $log.debug(event);
+                if (event && event.data) {
+                    // notify subscribers
+                    angular.forEach(subscribers, function (callback) {
+                        callback(JSON.parse(event.data));
+                    });
+                }
+            }
+
+            function onError(event) {
+                $log.debug('wsClient connection to [' + options.url + '] error');
+                $log.error(event);
+            }
         };
-
-        function connect() {
-            wsClient.socket = new WebSocket(wsClient.options.url);
-            wsClient.socket.onopen = onOpen;
-            wsClient.socket.onclose = onClose;
-            wsClient.socket.onmessage = onMessage;
-            wsClient.socket.onerror = onError;
-        }
-
-        function onOpen(event) {
-            $log.debug('wsClient connected to [' + wsClient.options.url + ']');
-            if (wsClient.reconnectTimeout) {
-                $timeout.cancel(wsClient.reconnectTimeout);
-            }
-        }
-
-        function onClose(event) {
-            $log.debug('wsClient connection to [' + wsClient.options.url + '] closed');
-            if (wsClient.reconnectTimeout) {
-                $timeout.cancel(wsClient.reconnectTimeout);
-            }
-            if (wsClient.options.reconnect) {
-                $log.debug('wsClient will try to reconnect in [' + wsClient.options.reconnectIntervalTimeout + '] ms');
-                wsClient.reconnectTimeout = $timeout(connect, wsClient.options.reconnectIntervalTimeout);
-            }
-        }
-
-        function onMessage(event) {
-            $log.debug('wsClient connection to [' + wsClient.options.url + '] message received, will notify listeners');
-            $log.debug(event);
-            if (event && event.data) {
-                // notify subscribers
-                angular.forEach(wsClient.subscribers, function (callback) {
-                    callback(JSON.parse(event.data));
-                });
-            }
-        }
-
-        function onError(event) {
-            $log.debug('wsClient connection to [' + wsClient.options.url + '] error');
-            $log.error(event);
-        }
-        
-        return wsClient;
     }
 
     angular.module('am.ws').factory('wsClient', wsClient);
